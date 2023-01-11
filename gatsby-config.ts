e25d24b7e9d6e39ca30c262599439c5fa513d8d9
LOFyz <leonardo_farias2010@hotmail.com>
2023-01-11 09:02:35 -0300

    feat: setup plugins in gatsby config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,27 +1,59 @@
 import type { GatsbyConfig } from "gatsby";
 
+import siteMetadata from "./src/config/metadata";
+
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `Evolucao Fitness Alunos`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }]
+    siteMetadata,
+    // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+    // If you use VSCode you can also use the GraphQL plugin
+    // Learn more at: https://gatsby.dev/graphql-typegen
+    graphqlTypegen: true,
+    plugins: [
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Evolução Fitness Alunos`,
+                short_name: `Alunos`,
+                start_url: `/`,
+                background_color: `#ffffff`,
+                theme_color: `#01534d`,
+                display: `standalone`,
+                icon: `src/images/icon.png`,
+                description: `Eu sou evolução!`,
+            },
+        },
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        "gatsby-plugin-sitemap",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+        {
+            resolve: "gatsby-plugin-nprogress",
+            options: {
+                color: "#01534d",
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+                trackingIds: ["GA-TRACKING_ID"],
+            },
+        },
+        {
+            resolve: `gatsby-plugin-page-creator`,
+            options: {
+                path: `${__dirname}/src/pages`,
+            },
+        },
+        "gatsby-plugin-offline",
+    ],
 };
 
 export default config;
