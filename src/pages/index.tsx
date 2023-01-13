bba00cc8b01fff93f199ffbb6f9adf415968cc36
LOFyz <leonardo_farias2010@hotmail.com>
2023-01-13 15:44:37 -0300

    fix: remember checkbox id

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -96,7 +96,7 @@ const Login: React.FC<PageProps> = (props) => {
                 <div className="flex items-start mb-6">
                     <div className="flex items-center h-5">
                         <input
-                            id="remember"
+                            id="keepSignedIn"
                             type="checkbox"
                             value=""
                             className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
@@ -105,7 +105,7 @@ const Login: React.FC<PageProps> = (props) => {
                         />
                     </div>
                     <label
-                        htmlFor="remember"
+                        htmlFor="keepSignedIn"
                         className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                     >
                         Manter autenticado
