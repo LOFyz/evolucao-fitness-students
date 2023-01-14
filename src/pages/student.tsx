d599fe0df8e1f79c09d7d7ecc8eeffe516b686e9
LOFyz <leonardo_farias2010@hotmail.com>
2023-01-14 02:36:34 -0300

    feat: reduce payment date field opacity when disabled

--- src/pages/student.tsx
+++ src/pages/student.tsx
@@ -189,7 +189,7 @@ const Student: React.FC<PageProps> = (props) => {
                             type="date"
                             name="lastPayment"
                             id="lastPayment"
-                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer disabled:opacity-50"
                             placeholder=" "
                             value={formik.values.lastPayment}
                             onChange={formik.handleChange}
@@ -197,7 +197,11 @@ const Student: React.FC<PageProps> = (props) => {
                         />
                         <label
                             htmlFor="lastPayment"
-                            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                            className={`peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 ${
+                                formik.values.paidToday
+                                    ? "opacity-50"
+                                    : "opacity-100"
+                            }`}
                         >
                             Ultimo pagamento
                         </label>
