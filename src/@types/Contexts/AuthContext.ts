edbd92f3f1622a87b89c5c80280bf052846e34ee
LOFyz <leonardo_farias2010@hotmail.com>
2023-01-19 15:22:58 -0300

    chore: improve authcontext types

--- src/@types/Contexts/AuthContext.ts
+++ src/@types/Contexts/AuthContext.ts
@@ -1,6 +1,14 @@
 import { UserCredential } from "firebase/auth";
 
 export type AuthContext = {
-    user: UserCredential["user"] | null;
-    signIn: (email: string, password: string) => void;
-}
+    user: User;
+    signIn: SignIn;
+};
+
+export type User = UserCredential["user"] | null;
+
+export type SignIn = (
+    email: string,
+    password: string,
+    remember?: boolean
+) => void;
