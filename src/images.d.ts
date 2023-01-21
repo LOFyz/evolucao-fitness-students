3e2fde8073346b92f61cbea70e0031887b66727c
LOFyz <leonardo_farias2010@hotmail.com>
2023-01-20 23:47:48 -0300

    feat: declare images to typescript

---
+++ src/images.d.ts
@@ -0,0 +1,8 @@
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.jfif";
+declare module "*.png";
+declare module "*.svg";
+declare module "*.gif";
+declare module "*.webp";
+declare module "*.ico";
