0a5d94a9762a03d0797c259b5bb206389b7ad171
LOFyz <leonardo_farias2010@hotmail.com>
2023-02-07 22:18:38 -0300

    feat: mutate function to list firebase hook fix: turn default data into []

--- src/hooks/useFirestoreList.ts
+++ src/hooks/useFirestoreList.ts
@@ -6,6 +6,7 @@ const db = getFirestore();
 export const useFirestoreList = (docName: string) => {
     const _collection = useMemo(() => collection(db, docName), [docName, db]);
     const [data, setData] = useState<any[]>([]);
+    const [update, setUpdate] = useState(0);
 
     useEffect(() => {
         getDocs(_collection).then((data) => {
@@ -16,7 +17,7 @@ export const useFirestoreList = (docName: string) => {
                 }))
             );
         });
-    }, [_collection]);
+    }, [_collection, update]);
 
-    return data;
+    return { data: data || [], mutate: () => setUpdate(update + 1) };
 };
